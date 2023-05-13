c7f293f34f058e4d7ed5b9978a74771986ae35bb
seada98 <mohamedseada1998@gmail.com>
2023-05-13 03:47:43 +0300

    Edit urls

--- Application/api/api/urls.py
+++ Application/api/api/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 from students.views import StudentsList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/students/', StudentsList.as_view())
+    path('api/students/', StudentsList.as_view()),
+    path('api/', include('students.urls')),
+
 ]
